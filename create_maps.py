ad212ee8d1e5b1ad95f63cc4226775e436b4a7c7
pjquinonez <pedrojesus@sbcglobal.net>
2021-07-21 15:54:16 -0700

    Added cross match to create_maps and did some redshift analysis with histograms

--- create_maps.py
+++ create_maps.py
@@ -15,10 +15,11 @@ from ligo.skymap.postprocess import find_greedy_credible_levels
 from datetime import datetime
 
 
-load_GLADE_db = False
+load_GLADE_db = True
 load_PanSTARRS1_db = True
-plot = False
-load_GW = False
+cross_match = True
+plot = True
+load_GW = True
 
 
 
@@ -94,7 +95,7 @@ if load_GW:
     for i in indexes:
         if i / npix >= perc_now / 100:
             print(perc_now, "%")
-            perc_now = perc_now + 5
+            perc_now = perc_now + 10
         if credible_levels[i] <= 0.90:
             gw_bools[i] = True
 
@@ -157,12 +158,7 @@ if load_PanSTARRS1_db:
     start = datetime.now()
     print("Loading PS1 Galaxy " + str(start.time()))
     db_query = '''
-    SELECT ra,PS1_Galaxy_v3.dec, z_phot, class, prob_Galaxy, objID FROM PS1_Galaxy_v3
-    WHERE 
-        ra >= 10.0 AND
-        ra <= 15.0 AND
-        PS1_Galaxy_v3.dec <= -22.0 AND
-        PS1_Galaxy_v3.dec >= -28.0;
+    SELECT ra,PS1_Galaxy_vNewLimits.dec, z_phot, class, prob_Galaxy, objID, gMeanKronMag FROM PS1_Galaxy_vNewLimits;
     '''
     PS1 = query_db([db_query])[0]
     PS1_ra = [x[0] for x in PS1]
@@ -171,21 +167,13 @@ if load_PanSTARRS1_db:
     PS1_class = [x[3] for x in PS1]
     PS1_prob_Galaxy = [x[4] for x in PS1]
     PS1_objid = [x[5] for x in PS1]
-    print("Unique Objects: " + str(len(np.unique(PS1_objid))))
-    print("Unique Objects as Percentage of Total: " + str(len(np.unique(PS1_objid))/len(PS1_objid)) + "%")
-    print("Galaxies: " + str(len([x for x in PS1_class if x == "GALAXY"])/len(PS1_class) * 100) + "%")
-
-
-    # print("Galaxy Probability: " + np.mean([float(x) for x in PS1_prob_Galaxy]))
-    sum = 0
-    for i in [PS1_prob_Galaxy[x] for x in range(len(PS1_prob_Galaxy)) if PS1_class[x] == "GALAXY"]:
-        sum = sum + i
-    print("Avg Galaxy Probability: " + str(sum/len([PS1_prob_Galaxy[x] for x in range(len(PS1_prob_Galaxy)) if PS1_class[x] == "GALAXY"])))
-    # print(len([x for x in PS1_prob_Galaxy if type(x) == float]))
-    # print(len(PS1_prob_Galaxy))
-
+    PS1_G_band = [x[6] for x in PS1]
+    print("Those with Galaxy Tag: " + str(len([x for x in PS1_class if x == "GALAXY"])/len(PS1_class) * 100) + "%")
     print("Len PS1 = " + str(len(PS1_ra)))
     print("Time to Complete: " + str(datetime.now() - start))
+
+
+
 
 ### DES
 if False:
@@ -228,7 +216,7 @@ if load_GLADE_db:
     start = datetime.now()
     print("Loading GLADE Galaxy " + str(start.time()))
     db_query = '''
-    SELECT RA, _DEC, z FROM GalaxyDistance2 
+    SELECT RA, _DEC, z, B FROM GalaxyDistance2 
     WHERE 
     
         (RA >= 10.0 AND
@@ -245,22 +233,81 @@ if load_GLADE_db:
     GLADE_ra = [x[0] for x in GLADE]
     GLADE_dec = [x[1] for x in GLADE]
     GLADE_z = [x[2] for x in GLADE]
+    GLADE_B_band = [x[3] for x in GLADE]
     print("Len GLADE = " + str(len(GLADE_ra)))
     print("Time to Complete: " + str(datetime.now() - start))
+
+### Crossmatch of GLADE & PS1
+if cross_match:
+    start = datetime.now()
+    print("Starting Cross Match - " + str(start.time()))
+    nums = len(GLADE_ra)
+    cross_match = [[] for i in range(nums)]
+    dist_limit = 1 / 3600 # 1/3600 is 1 arcsecond in degrees
+    print("Distance Limit: " + str(dist_limit * 3600) + " arcseconds")
+    for i in range(nums):
+        right = GLADE_ra[i] + dist_limit
+        left = GLADE_ra[i] - dist_limit
+        lower = GLADE_dec[i] - dist_limit
+        upper = GLADE_dec[i] + dist_limit
+        local_PS1_galaxies = [x for x in range(len(PS1_objid)) if PS1_ra[x] <= right and PS1_ra[x] >= left and PS1_dec[x] >= lower and PS1_dec[x] <= upper]
+        for ps1_index in local_PS1_galaxies:
+            dist = np.sqrt(((GLADE_ra[i] - PS1_ra[ps1_index]) ** 2) + ((GLADE_dec[i] - PS1_dec[ps1_index]) ** 2))
+            if dist < dist_limit:
+                cross_match[i] = cross_match[i] + [PS1_objid[ps1_index]]
+    print("Finished Cross Match: " + str(datetime.now() - start))
+    print("Number of Cross Matches: " + str(len([x for x in cross_match if len(x) > 0])))
+    print("Number of Cross Matches above 1: " + str(len([x for x in cross_match if len(x) > 1])))
+
+    start = datetime.now()
+    print("Limiting PS1 Data and Making Histogram of Differences - " + str(start.time()))
+    GLADE_indexs = [x for x in range(len(cross_match)) if len(cross_match[x]) > 0]
+    only_cross = [x for x in cross_match if len(x) > 0]
+    PS1_index = [[x for x in range(len(PS1_objid)) if PS1_objid[x] == only_cross[y][0]][0] for y in range(len(only_cross))]
+    band_diff = [abs(PS1_G_band[PS1_index[x]] - GLADE_B_band[GLADE_indexs[x]]) for x in range(len(GLADE_indexs)) if (type(PS1_G_band[PS1_index[x]]) == float and type(GLADE_B_band[GLADE_indexs[x]]) == float)]
+    z_diff = [abs(PS1_z[PS1_index[x]] - GLADE_z[GLADE_indexs[x]]) for x in range(len(GLADE_indexs)) if (type(PS1_z[PS1_index[x]]) == float and type(GLADE_z[GLADE_indexs[x]]) == float)]
+    cross_GLADE_z = [GLADE_z[GLADE_indexs[x]] for x in range(len(GLADE_indexs)) if type(GLADE_z[GLADE_indexs[x]]) == float]
+    cross_PS1_z = [PS1_z[PS1_index[x]] for x in range(len(PS1_index)) if type(PS1_z[PS1_index[x]]) == float and PS1_z[PS1_index[x]] >=0 and PS1_z[PS1_index[x]] <= 1]
+
+    plt.figure(1)
+    plt.hist([x for x in band_diff if x < 100], bins=20)
+    plt.title("Histogram of B-band and G-band difference of GLADE and PS1 Cross Match")
+    plt.xlabel("Band Difference (mags)")
+    plt.ylabel("Frequency of difference")
+    plt.savefig("images/Crossmatch Band diff.png", bbox_inches="tight", dpi=300)
+
+    plt.figure(5)
+    print("GLADE cross Z Mean: " + str(np.mean(cross_GLADE_z)))
+    print("PS1 cross Z Mean: " + str(np.mean(cross_PS1_z)))
+    plt.hist([x for x in z_diff if x < 0.25], bins=20)
+    plt.title("Histogram of Red Shift difference of GLADE and PS1 Cross Match")
+    plt.xlabel("Red Shift Difference")
+    plt.ylabel("Frequency of difference")
+    plt.savefig("images/Red Shift diff.png", bbox_inches="tight", dpi=300)
+
+    ### Get Rid of Cross Match PS1 Galaxies
+
+    PS1_ra = [PS1_ra[x] for x in range(len(PS1_ra)) if x not in PS1_index]
+    PS1_dec = [PS1_dec[x] for x in range(len(PS1_dec)) if x not in PS1_index]
+    PS1_z = [PS1_z[x] for x in range(len(PS1_z)) if x not in PS1_index]
+    PS1_class = [PS1_class[x] for x in range(len(PS1_class)) if x not in PS1_index]
+    PS1_prob_Galaxy = [PS1_prob_Galaxy[x] for x in range(len(PS1_prob_Galaxy)) if x not in PS1_index]
+    PS1_objid = [PS1_objid[x] for x in range(len(PS1_objid)) if x not in PS1_index]
+    PS1_G_band = [PS1_G_band[x] for x in range(len(PS1_G_band)) if x not in PS1_index]
+    print("New PS1 Len: " + str(len(PS1_ra)))
+    print("Finish Limiting PS1 Data: " + str(datetime.now() - start))
+
 
 
 ### SKY MAP/HISTOGRAM
 if plot:
     start = datetime.now()
     print("Start Plotting " + str(start.time()))
-
-
+    plt.figure(2)
     map = Basemap(width=3*(10**6),height=3*(10**6)*0.75,projection='lcc',
                 resolution='c',lat_0=np.mean(gw_dec),lon_0=np.mean(gw_ra))
     # map = Basemap(projection='lcc',
     #               resolution='c', lat_0=np.mean(gw_dec), lon_0=np.mean(gw_ra))
-
-
     # map.drawmapboundary(fill_color='aqua')
     # map.fillcontinents(color='coral',lake_color='aqua')
     # map.drawcoastlines()
@@ -294,16 +341,18 @@ if plot:
     plt.savefig("images/Zoomed Map_inProgress.png", bbox_inches = "tight", dpi = 300)
 
     ## Red shift Histogram - PS1
-    plt.figure(5)
-    plt.hist([x for x in PS1_z if 0<x<=1], bins=20)
+    plt.figure(3)
+    # plt.hist([x for x in PS1_z if 0<x<=1], bins=20)
+    plt.hist([x for x in PS1_z if -0.5<x<=1], bins=20)
     plt.title("Histogram of Redshifts from PanSTARRS1")
     plt.xlabel("Photometric Red Shift")
     plt.ylabel("Frequency of Galaxies")
     plt.savefig("images/Z Hist PS1_inProgress.png", bbox_inches = "tight", dpi = 300)
 
     ## Red shift Histogram - PS1
-    plt.figure(6)
-    plt.hist([x for x in GLADE_z if 0 < x <= 1], bins=20)
+    plt.figure(4)
+    # plt.hist([x for x in GLADE_z if 0 < x <= 1], bins=20)
+    plt.hist([x for x in GLADE_z if 0 < x <= 1.25], bins=20)
     plt.title("Histogram of Redshifts from GLADE")
     plt.xlabel("Spectroscopic Red Shift")
     plt.ylabel("Frequency of Galaxies")
